82a385bcc4e93343b02ce59412b6184c3182bb33
Raphael Floresca <rpngfl@gmail.com>
2024-06-07 00:00:54 +0800

    feat: scaffold exercise list

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,13 @@
-import { StyleSheet, ScrollView, View, Text } from 'react-native';
+import { StyleSheet, ScrollView, View, Text } from 'react-native'
 
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { LinearGradient } from 'expo-linear-gradient';
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { TabBarIcon } from '@/components/navigation/TabBarIcon'
+import { LinearGradient } from 'expo-linear-gradient'
 
 export default function HomeScreen() {
   return (
     <SafeAreaView style={styles.container}>
+      {/* Header */}
       <View style={styles.header}>
         <TabBarIcon name="arrow-back" style={styles.icon} />
         <View style={styles.iconsRight}>
@@ -14,11 +15,14 @@ export default function HomeScreen() {
           <TabBarIcon name="cog-outline" style={styles.icon} />
         </View>
       </View>
+      {/* Content */}
       <ScrollView>
+        {/* Title */}
         <View style={styles.titleContainer}>
           <Text style={{ fontSize: 28, fontWeight: 'bold' }}>Full Body</Text>
           <Text>With Candice</Text>
         </View>
+        {/* Free Trial Banner */}
         <LinearGradient colors={['#5B167C', '#6D179C']} style={styles.trialBanner}>
           <View style={styles.trialBannerText}>
             <Text style={{ color: 'white' }}>Your 7-day free trial is running. Enjoy and make the most of it!</Text>
@@ -28,8 +32,50 @@ export default function HomeScreen() {
             <Text style={{ color: 'white', textAlign: 'center' }}>days left</Text>
           </View>
         </LinearGradient>
+        {/* Triset */}
+        <View style={styles.exercisesContainer}>
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+            <View style={{ flexDirection: 'row', gap: 10 }}>
+              <View style={{ width: 80, height: 100, backgroundColor: 'red', borderRadius: 22 }}></View>
+              <View style={{ justifyContent: 'center' }}>
+                <Text>Single Arm Cable Row (Left)</Text>
+                <Text>4 sets x 10-12 reps</Text>
+              </View>
+            </View>
+            <View style={{ flexDirection: 'row', alignItems: 'center', gap: 16 }}>
+              <TabBarIcon name="refresh-circle" />
+              <TabBarIcon name="ellipsis-horizontal" />
+            </View>
+          </View>
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+            <View style={{ flexDirection: 'row', gap: 10 }}>
+              <View style={{ width: 80, height: 100, backgroundColor: 'red', borderRadius: 22 }}></View>
+              <View style={{ justifyContent: 'center' }}>
+                <Text>Single Arm Cable Row (Right)</Text>
+                <Text>4 sets x 10-12 reps</Text>
+              </View>
+            </View>
+            <View style={{ flexDirection: 'row', alignItems: 'center', gap: 16 }}>
+              <TabBarIcon name="refresh-circle" />
+              <TabBarIcon name="ellipsis-horizontal" />
+            </View>
+          </View>
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+            <View style={{ flexDirection: 'row', gap: 10 }}>
+              <View style={{ width: 80, height: 100, backgroundColor: 'red', borderRadius: 22 }}></View>
+              <View style={{ justifyContent: 'center' }}>
+                <Text>Cable Seated Row</Text>
+                <Text>4 sets x 6-8 reps</Text>
+              </View>
+            </View>
+            <View style={{ flexDirection: 'row', alignItems: 'center', gap: 16 }}>
+              <TabBarIcon name="refresh-circle" />
+              <TabBarIcon name="ellipsis-horizontal" />
+            </View>
+          </View>
+        </View>
       </ScrollView>
-    </SafeAreaView>
+    </SafeAreaView >
   );
 }
 
@@ -81,7 +127,7 @@ const styles = StyleSheet.create({
   trialBannerText: {
     paddingLeft: 25,
     paddingVertical: 20,
-    flex: 1,
+    flex: 5,
     justifyContent: 'center'
   },
   trialDaysRemaining: {
@@ -90,5 +136,13 @@ const styles = StyleSheet.create({
     paddingRight: 25,
     flex: 1,
     justifyContent: 'center'
+  },
+  exercisesContainer: {
+    marginHorizontal: 10,
+    borderWidth: 1,
+    borderColor: '#E9E9E9',
+    borderRadius: 28,
+    padding: 6,
+    gap: 12
   }
 });
