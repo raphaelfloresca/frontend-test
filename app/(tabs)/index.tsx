4be8dc53e0dd5ef434bce0425d82ebb7bde58a8e
Raphael Floresca <rpngfl@gmail.com>
2024-06-06 22:27:59 +0800

    feat: add styling to header and scroll view

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,11 +5,13 @@ import { TabBarIcon } from '@/components/navigation/TabBarIcon';
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <View style={styles.header}>
-        <TabBarIcon name="arrow-back" />
-        <TabBarIcon name="heart" />
-        <TabBarIcon name="cog-outline" />
+        <TabBarIcon name="arrow-back" style={styles.icon} />
+        <View style={styles.iconsRight}>
+          <TabBarIcon name="heart" style={[styles.icon, styles.iconHeart]} />
+          <TabBarIcon name="cog-outline" style={styles.icon} />
+        </View>
       </View>
       <ScrollView style={styles.content}>
         {[...Array(100)].map((x, i) =>
@@ -21,12 +23,39 @@ export default function HomeScreen() {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    flex: 1
+  },
   header: {
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 15,
+    height: 56,
+    backgroundColor: '#fff'
+  },
+  iconsRight: {
+    display: 'flex',
+    flexDirection: 'row',
+    gap: 4
+  },
+  icon: {
+    padding: 12,
+    color: '#262C45'
+  },
+  iconHeart: {
+    color: '#F6727B'
   },
   content: {
-    flexGrow: 1
+    flexGrow: 1,
+    backgroundColor: '#fff',
+  },
+  titleContainer: {
+    paddingHorizontal: 40,
+    display: 'flex',
+    gap: 12
   }
 });
