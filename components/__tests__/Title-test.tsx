2a82298fedbfc005301a2c7e8c441d3ff169a18d
Raphael Floresca <rpngfl@gmail.com>
2024-06-10 13:16:23 +0800

    feat: add icon with useAssets

--- components/__tests__/Title-test.tsx
+++ components/__tests__/Title-test.tsx
@@ -1,21 +1,30 @@
 import * as React from 'react'
 import { render } from '@testing-library/react-native';
 import Title from '../ui/Title';
+import { Asset } from 'expo-asset';
+
+// Mock the useFonts hook
+jest.mock('expo-asset', () => ({
+  ...jest.requireActual('expo-asset'),
+  useAssets: jest.fn(() => []),
+}));
 
 describe('Title', () => {
-  it('renders correctly', () => {
-    const { getByTestId } = render(<Title titleText="Full Body" />)
-    const element = getByTestId("test-title")
+  it('renders correctly', async () => {
+    const { findByTestId } = render(<Title titleText="Full Body" />)
+    const element = await findByTestId("test-title")
     expect(element).toBeOnTheScreen();
   });
 
-  it('has children', () => {
-    const { getByTestId } = render(<Title titleText="Full Body" />)
-    expect(getByTestId("test-title")).not.toBeEmptyElement();
+  it('has children', async () => {
+    const { findByTestId } = render(<Title titleText="Full Body" />)
+    const element = await findByTestId("test-title")
+    expect(element).not.toBeEmptyElement();
   });
 
-  it('renders title correctly when titleText prop is passed', () => {
-    const { getByTestId } = render(<Title titleText="Leg Day" />)
-    expect(getByTestId("test-title")).toHaveTextContent(/Leg Day/)
+  it('renders title correctly when titleText prop is passed', async () => {
+    const { findByTestId } = render(<Title titleText="Leg Day" />)
+    const element = await findByTestId("test-title")
+    expect(element).toHaveTextContent(/Leg Day/)
   });
 });
--- components/ui/Title.tsx
+++ components/ui/Title.tsx
@@ -1,16 +1,24 @@
 import React from 'react'
+import { useAssets } from 'expo-asset';
 import { Text, View, StyleSheet } from 'react-native'
+import { Image } from 'expo-image'
 
 type TitleProps = {
   titleText: string
 }
 
 export default function Title({ titleText }: TitleProps) {
+  const [assets, error] = useAssets([
+    require('../../assets/images/candice-icon.png'),
+  ]);
 
   return (
     <View testID="test-title" style={styles.titleContainer} >
       <Text style={styles.titleHeading}>{titleText}</Text>
-      <Text style={styles.byLine}>With Candice</Text>
+      <View style={styles.byLineContainer}>
+        {assets ? <Image source={{ uri: assets[0].uri }} style={styles.icon} /> : null}
+        <Text style={styles.byLine}>With Candice</Text>
+      </View>
     </View>
   )
 }
@@ -24,10 +32,19 @@ const styles = StyleSheet.create({
     fontFamily: 'WorkSans_600SemiBold',
     fontSize: 28,
   },
+  byLineContainer: {
+    flexDirection: 'row',
+    gap: 12,
+    alignItems: 'center'
+  },
   byLine: {
     fontFamily: 'OpenSans_400Regular',
     fontSize: 12,
     textTransform: 'uppercase',
     letterSpacing: 2
+  },
+  icon: {
+    width: 28,
+    height: 28
   }
 });
